40e19b14bb473165009cfe05645e0bcdcab819eb
kunal singh <kunal.singh@canoo.com>
2017-01-24 16:51:58 +0530

    Add saucelab configuration

--- sauce.launchers.js
+++ sauce.launchers.js
@@ -1,52 +1,190 @@
 exports.browsers = [
     {
-        platform: 'Windows 7',
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'Windows XP',
+        version: '46.0'
+    },
+    {
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        platform: 'Windows XP',
+        version: '42.0'
+    },
+    {
+        base: 'SauceLabs',
         browserName: 'internet explorer',
+        platform: 'Windows 7',
+        version: '9.0'
+    },
+
+    {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 7',
+        version: '10.0'
+    },
+    {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 7',
+        version: '11.0'
+    },
+
+    {
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        platform: 'Windows 7',
+        version: '42.0'
+    },
+    {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'Windows 7',
+        version: '46.0'
+    },
+    {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 8',
+        version: '10.0'
+    },
+
+    {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'Windows 8',
+        version: '46.0'
+    },
+    {
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        platform: 'Windows 8',
+        version: '42.0'
+    },
+    {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 8.1',
         version: '11.0'
     },
     {
-        platform: 'Windows 7',
-        browserName: 'chrome'
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'Windows 8.1',
+        version: '46.0'
     },
     {
-        platform: 'Windows 7',
-        browserName: 'firefox'
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        platform: 'Windows 8.1',
+        version: '42.0'
     },
 
     {
-        platform: 'Windows 8',
-        browserName: 'chrome'
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'OS X 10.11',
+        version: '46.0'
     },
     {
-        platform: 'Windows 8',
-        browserName: 'firefox'
-    },
-
-    {
-        platform: 'Windows 8.1',
-        browserName: 'internet explorer',
-        version: '11.0'
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        platform: 'OS X 10.11',
+        version: '42.0'
     },
     {
-        platform: 'Windows 8.1',
-        browserName: 'chrome'
-    },
-    {
-        platform: 'Windows 8.1',
-        browserName: 'firefox'
+        base: 'SauceLabs',
+        browserName: 'safari',
+        platform: 'OS X 10.11',
+        version: '9.0'
     },
 
     {
-        platform: 'Windows 10',
-        browserName: 'internet explorer'
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'OS X 10.10',
+        version: '46.0'
     },
     {
-        platform: 'Windows 10',
-        browserName: 'chrome'
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        platform: 'OS X 10.10',
+        version: '42.0'
     },
     {
-        platform: 'Windows 10',
-        browserName: 'firefox'
+        base: 'SauceLabs',
+        browserName: 'safari',
+        platform: 'OS X 10.10',
+        version: '8.0'
+    },
+
+    {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'Linux',
+        version: '46.0'
+    },
+    {
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        platform: 'Linux',
+        version: '42.0'
+    },
+    {
+        base: 'SauceLabs',
+        browserName: 'opera',
+        platform: 'Linux',
+        version: '12.15'
+    },
+
+    {
+        base: 'SauceLabs',
+        browserName: 'Safari',
+        appiumVersion: '1.4.16',
+        deviceName: 'iPad Simulator',
+        deviceOrientation: 'portrait',
+        platformName: 'iOS',
+        platformVersion: '8.4'
+    },
+    {
+        base: 'SauceLabs',
+        browserName: 'Browser',
+        appiumVersion: '1.4.16',
+        deviceName: 'Android Emulator',
+        deviceOrientation: 'portrait',
+        platformName: 'Android',
+        platformVersion: '5.1'
+    }
+    ,
+    {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'OS X 10.9',
+        version: '46.0'
+    }
+    ,
+    {
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        platform: 'OS X 10.9',
+        version: '42.0'
+    },
+    {
+        base: 'SauceLabs',
+        browserName: 'safari',
+        platform: 'OS X 10.9',
+        version: '7.0'
+    },
+    {
+        base: 'SauceLabs',
+        browserName: 'Safari',
+        appiumVersion: '1.6.3',
+        deviceName: 'iPad Simulator',
+        deviceOrientation: 'portrait',
+        platformName: 'iOS',
+        platformVersion: '8.3',
+        flags: ['--disable-web-security']
     }
     // ,
     // {
