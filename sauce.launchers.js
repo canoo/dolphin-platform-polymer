79c4adbe945530ae2ff690ee81e1d8ae2b34b22a
kunal singh <kunal.singh@canoo.com>
2017-02-01 13:20:30 +0530

    Add more browsers for saucelabs test

--- sauce.launchers.js
+++ sauce.launchers.js
@@ -49,13 +49,72 @@ exports.browsers = [
         browserName: "safari",
         platform: "OS X 10.11",
         version: "9"
+    },
+    {
+        platformName: 'iOS',
+        platformVersion: '8.4',
+        deviceName: 'iPad Simulator',
+        browserName: 'Safari',
+        deviceOrientation: 'landscape'
+    },
+    {
+        platformName: 'iOS',
+        platformVersion: '9.2',
+        deviceName: 'iPad Simulator',
+        browserName: 'Safari',
+        deviceOrientation: 'landscape'
+    },
+    {
+        platformName: 'iOS',
+        platformVersion: '9.3',
+        deviceName: 'iPad Simulator',
+        browserName: 'Safari',
+        deviceOrientation: 'landscape'
+    },
+
+    {
+        platform: 'Linux',
+        browserName: 'chrome'
+    },
+    {
+        platform: 'Linux',
+        browserName: 'firefox'
+    },
+
+    {
+        platform: 'OS X 10.10',
+        browserName: 'chrome'
+    },
+    {
+        platform: 'OS X 10.10',
+        browserName: 'firefox'
+    },
+    {
+        platform: 'OS X 10.10',
+        browserName: 'safari'
+    },
+
+    {
+        platform: 'OS X 10.11',
+        browserName: 'chrome'
+    },
+    {
+        platform: 'OS X 10.11',
+        browserName: 'firefox'
+    },
+    {
+        platform: 'OS X 10.11',
+        browserName: 'safari'
+    },
+
+    {
+        platform: 'Windows 7',
+        browserName: 'internet explorer',
+        version: '10.0'
+    },
+    {
+        platform: 'Windows 8',
+        browserName: 'internet explorer',
+        version: '10.0'
     }
-    // ,
-    // {
-    //     platform: 'iOS',
-    //     version: '8.4',
-    //     device: 'iPad Simulator',
-    //     browserName: 'Safari',
-    //     deviceOrientation: 'landscape'
-    // }
 ];
